985f180d729ba4651dad34ef1d16b88d0ee717a4
Leo Martel <leo@lpm.io>
2018-05-31 15:22:28 -0700

    add init and framework code

---
+++ src/meso-utils.el
@@ -0,0 +1,119 @@
+;;; meso-utils.el --- Helper functions for writing Meso modules, and modern Emacs Lisp generally.   -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Leo Martel
+
+;; Author: Leo Martel <leo@lpm.io>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Helpers in this file should have no or few dependencies.
+;; Complicated functionality deserves its own module!
+
+;; The meso-- prefix with two dashes signifies a "private" function,
+;; designed to be called from module implementation code rather than
+;; by user configuration.
+
+;;; Code:
+
+(require 'use-package)
+
+;; Modern collection, file and string manipulation libs
+(use-package dash)
+(use-package f)
+(use-package s)
+
+(defun meso/shell (command)
+  "Run shell COMMAND and return trimmed output."
+  (s-chomp (shell-command-to-string command)))
+
+(defmacro meso/install-theme (theme)
+  "Install, but do not load, theme THEME. This lets you build a library of \
+themes to choose between in customize-theme without enabling all of them at once."
+  `(let ((packaged-themes-dir (meso--f-join-dir custom-theme-directory "elpa")))
+     (add-to-list 'custom-theme-load-path 
+                  (meso--package-download ,theme packaged-themes-dir))))
+
+
+(defmacro meso--require-set (var)
+  "Assert symbol VAR is defined, and crash with a clear error message if it isn't."
+  `(unless (boundp (quote ,var))
+     (error "Error loading file %s: module expected variable `%s' to be defined, but got void or nil"
+            ,(f-filename (f-this-file)) (quote ,var))))
+
+(defmacro meso--set-if-unset (var value &optional desc)
+  "If VAR is not bound, set its value globally to VALUE."
+  `(unless (boundp (quote ,var))
+     (defvar ,var ,value ,desc)))
+
+(defun meso--f-join-dir (&rest args)
+  "Join ARGS to a single path with f-join, but ensure exactly one trailing slash at the end."
+  (s-append "/" (s-chop-suffix "/ "(apply 'f-join args))))
+
+(defun meso--noop (&rest args)
+  "A simple function that accepts any number of arguments and does nothing."
+  nil)
+
+(defmacro meso--let-noop (fns &rest body)
+  "Override all functions FNS to do nothing while executing &BODY, then \
+restore their previous functionality."
+  `(progn
+     (dolist (fn ,fns)
+       (message "advising %s" fn)
+       (advice-add fn :override #'meso--noop))
+     ,@body
+     (dolist (fn ,fns)
+       (advice-remove fn #'meso--noop))))
+
+(defmacro meso--package-download (name dir &optional force-update)
+  "Leverage package.el to download package NAME into directory DIR *without* loading it. Useful for themes.\
+You must specify a non-default directory, because `package-initialize' will auto-load packages from the default directory. This will not reinstall packages that are already installed, unless FORCE-UPDATE is non-nil. Returns the installed package directory path."
+  `(let ((old-package-dir package-user-dir)
+         (old-package-alist package-alist))
+
+     ;; Scan the target directory for packages in order to detect whether the package is already installed.
+     (setq package-user-dir ,dir)
+     ;; (package-initialize :no-activate)
+     ;; (package-refresh-contents)
+     (when (or ,force-update
+               ;; (not (package-installed-p (quote ,name))) ;; TODO too slow
+               (not (f-glob (format "%s-*" (quote ,name)) ,dir))
+               )
+       
+       ;; These functions are pulled from `package-unpack'. We want to let package.el download and unpack the package
+       ;; as well as generate autoloads etc, but prevent it from "activating" (loading).
+       ;; We also skip compilation, which relies on the package being "activated."
+       ;; This code is hacky and brittle, because it relies on the internals of package.el.
+       ;; If this breaks, look at `package-unpack'.
+       (meso--let-noop '(package-activate-1
+                         package--compile
+                         package--load-files-for-activation)
+                       (if ,force-update
+                           (package-reinstall (quote ,name))
+                         (package-install (quote ,name) :dont-select))))
+
+     ;; Clean up after ourselves: reset installed package list and package directory to their previous, proper values
+     (setq package-alist old-package-alist)
+     (setq package-user-dir old-package-dir)
+     
+     (let ((pkg-dir (car (-sort #'string> (f-glob (format "%s-*" (quote ,name)) ,dir)))))
+
+       ;; We *do* want the package on the load-path, so we can `require' it later.
+       ;; We just don't want to load it *now*. This is normally handled during "activation."
+       (add-to-list 'load-path pkg-dir)
+       pkg-dir)))
+
+(provide 'meso-utils)
+;;; meso-utils.el ends here
---
+++ src/meso.el
@@ -0,0 +1,178 @@
+;;; meso.el --- Bootstrap the package and module systems.   -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Leo Martel
+
+;; Author: Leo Martel <leo@lpm.io>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file bootstraps modern package and module management tools.
+;; We also set up package hygene: missing packages are installed automatically,
+;; and unneeded packages are deleted automatically.
+
+;; This is the most dense file within Meso, because it sets up the simple helpers
+;; used everywhere else. You shouldn't need to modify this file.
+
+;;; Code:
+
+;; Force a modern-ish version of Emacs
+(when (< emacs-major-version 25)
+  (error "Emacs version (%s) too old for Meso compatibility, stopping" emacs-version))
+
+;; Add ~/.emacs.d/src/ and *all recursive descendants* to load-path.
+(dolist (srcs-dir-name (list "src/"
+			     ;; add more source directories here if needed!
+			     ))
+  (let ((default-directory (concat user-emacs-directory srcs-dir-name)))
+    (add-to-list 'load-path default-directory)
+    (normal-top-level-add-subdirs-to-load-path)))
+
+;; Initialize the package system, and add useful package repositories.
+(require 'package)  
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; Bootstrap package list on first run.
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; In addition, automatically refresh package indices once per session, lazily.
+(let ((package-contents-refreshed nil))
+  (defun package-refresh-contents-once (&rest ignored)
+    "Run package-refresh-contents if it hasn't been run before this session."
+    (unless package-contents-refreshed
+      (package-refresh-contents)))
+
+  (defun set-package-contents-refreshed (&rest ignored)
+    (setq package-contents-refreshed t))
+
+  (advice-add 'package-refresh-contents :after #'set-package-contents-refreshed)
+  (advice-add 'package-install :before 'package-refresh-contents-once)
+  (advice-add 'package-list-packages :before 'package-refresh-contents-once))
+
+
+;; Bootstrap use-package for installing and configuring packages.
+(when (not (package-installed-p 'use-package))
+  (package-install 'use-package))
+(require 'use-package)
+(setq-default use-package-always-ensure t) ; Always install missing packages.
+
+;; Enhanced use-package functionality: let us require and auto-install system packages (command line tools) as well.
+(use-package system-packages)
+(use-package use-package-ensure-system-package)
+
+
+;; List of symbols to ignore in dependency checks.
+;; Add a package name to this list to prevent it from being automatically downloaded by use-package.
+;; This is rarely needed, but (for example) it enables using `org-plus-contrib' as a drop-in replacement
+;; for `org' without the latter being downloaded as well.
+(let ((package-dependency-blacklist ()))
+
+  (defun meso/package-dependency-check-ignore (pkg)
+    "Don't install PKG in package-install dependency checks when another package requests it, if you know better than the package does."
+    (add-to-list 'package-dependency-blacklist pkg))
+
+  ;; from https://emacs.stackexchange.com/questions/26507/use-org-plus-contrib-to-satisfy-dependency-of-org2blog/26513#26513
+  (defun meso/--package-dependency-check-ignore (orig-ret)
+    "Remove the `black listed packages' from ORIG-RET.
+
+Packages listed in the let-bound `package-dependency-blacklist' will not be auto-installed
+even if they are found as dependencies.
+
+It is known that this advice is not effective when installed packages
+asynchronously using `paradox'. Below is effective on synchronous
+package installations."
+    (let (new-ret
+          pkg-name)
+      (dolist (pkg-struct orig-ret)
+        (setq pkg-name (package-desc-name pkg-struct))
+        (if (member pkg-name package-dependency-blacklist)
+            (message (concat "Package `%s' will not be installed. "
+                             "See `meso/package-dependency-check-ignore'.")
+                     pkg-name)
+          (push pkg-struct new-ret)))
+      ;; It's *very* critical that the order of packages stays the same in NEW-RET
+      ;; as in ORIG-RET. The `push' command flips the order, so use `reverse'
+      ;; to flip the order back to the original.
+      ;;   Without this step, you will get package activation errors when
+      ;; installing packages with dependencies.
+      (setq new-ret (reverse new-ret))
+      new-ret))
+  (advice-add 'package-compute-transaction :filter-return #'meso/--package-dependency-check-ignore))
+
+
+;; Set up hooks for package-safe-delete to delete unused packages automatically.
+;; This way you can manually install packages for testing and just restart to clean up.
+;; If you like the package, add it to your init file or to a module.
+(use-package package-safe-delete
+  :demand t 
+  :config
+  (mapcar (lambda (package-name) (add-to-list 'package-safe-delete-required-packages package-name))
+          '(paradox use-package package-safe-delete))
+  (advice-add #'use-package 
+              :around 
+              (lambda (old-function package-name &rest options)
+                "Add packages to the package-safe-delete required packages list when loaded with use-package"
+                (let ((ensured (plist-get options ':ensure)))
+
+                  ;; If ensured different package from package-name, protect that one instead
+                  (if (not (booleanp ensured))
+                      (add-to-list 'package-safe-delete-required-packages ensured)
+                    (add-to-list 'package-safe-delete-required-packages package-name)))
+                (apply old-function (cons package-name options)))))
+
+
+;; Finally, set up the Meso module system. By default, you must load or explicitly skip all modules,
+;; to ensure that you know what's available and that modules don't go missing and cause confusion.
+(require 'meso-utils)
+
+(meso--set-if-unset meso/force-explicit-skip-module t
+  "If non-nil, unwanted meso modules must be explicitly skipped with meso/skip-module.")
+
+
+;; This cannot be bound to a clojure with `let' because macros use the lexical scope of their
+;; *call site*, where they're compiled; not the scope where they're defined.
+;; I learned here that defmacro inside let doesn't do what you want.
+;; https://emacs.stackexchange.com/questions/5416/how-is-the-variable-scoping-for-macros-determined
+(setq meso--referenced-modules ())
+
+(defmacro meso/load-module (name)
+  "Acknowledge and load source file: module-NAME.el."
+  `(let ((module (intern (format "module-%s" (quote ,name)))))
+     (require module)
+     (add-to-list 'meso--referenced-modules module)))
+
+(defmacro meso/skip-module (name)
+  "Acknowledge, but do not load, source file: module-NAME.el."
+  `(let ((module (intern (format "module-%s" (quote ,name)))))
+     (add-to-list 'meso--referenced-modules module)))
+
+(defun meso--audit-modules ()
+  "After all initialization is finished, assert that all modules have been explicitly loaded or skipped."
+  (if meso/force-explicit-skip-module
+      (let* ((all-modules (f-entries (meso--f-join-dir user-emacs-directory "src" "module") (lambda (file) (s-ends-with? ".el" file)) :recursive))
+	     (all-module-names (--map (intern (s-chop-suffix ".el" (f-filename it))) all-modules))
+	     (missing-modules (-difference all-module-names meso--referenced-modules)))
+	(if missing-modules
+	    (warn "[Meso] Modules found in src/modules but omitted in init.el: %s. Must load with `load-module' \
+or explicitly skip with `skip-module', or disable this warning by adding \
+`(setq meso/force-explicit-skip-module nil)' to your init.el file.'." missing-modules)))))
+
+(add-hook 'after-init-hook #'meso--audit-modules)
+
+(provide 'meso)
+;;; meso.el ends here
