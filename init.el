7175e1fcb34464f07d15942db9cff1c90b3a6c78
Leo Martel <leo@lpm.io>
2018-05-31 15:22:28 -0700

    add init and framework code

---
+++ init.el
@@ -0,0 +1,80 @@
+;;; init.el --- This is the customization file emacs looks for on startup, that loads everything else.   -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; What is meso: TODO
+;; How to customize: TODO
+
+;;; Code:
+
+(defun meso/user-init ()
+  "This initialization function runs before all other setup. \
+This is a good place to change how that setup happens, \
+e.g. by changing file paths or variables that are read at load time. \
+When in doubt, add your code in meso/user-config, not here."
+
+  (setq debug-on-error t) ; Show a stack trace when an error is raised. This is built into Emacs.
+
+  ;; Settings with the user/ prefix are custom settings used by Meso and its modules.
+  (setq user/full-name "Leo Martel")
+  (setq user/email "leo@lpm.io")
+  (setq user/appearance/font "Fira Code Retina 13")
+
+  ;; Your init code here!
+  
+  
+  )
+
+(defun meso/user-modules ()
+  "Choose modules to load. \
+Load modules with `(meso/load-module module-name)' or skip modules with `(meso/skip-module module-name)'. \
+You need to load or explicitly skip every module; to disable this, add `(setq meso/force-explicit-skip-module nil)` \
+to your meso/user-init function."
+
+  (meso/load-module appearance)
+  (meso/load-module backups)
+  (meso/load-module better-defaults)
+  (meso/load-module code)
+  (meso/load-module gui)
+  (meso/load-module helm)
+  (meso/load-module help)
+  (meso/load-module ide)
+  (meso/load-module lisp)
+  (meso/load-module mac)
+  (meso/load-module orgmode)
+  (meso/load-module pandoc)
+  (meso/load-module persistence)
+  (meso/load-module projects)
+  (meso/load-module text)
+  (meso/load-module theme-samples)
+  (meso/load-module windows)
+
+  (meso/load-module lang-emacs-lisp))
+
+
+(defun meso/user-config ()
+  "This configuration function runs after all other setup. \
+This is a good place to customize behavior, e.g. by overriding \
+variable defaults, adding new keybinds or hooks, etc. \
+When in doubt, add your code here."
+
+  ;; Your config code here!
+  
+  
+  )
+
+(defun meso/init ()
+  "Run user and framework init code. You shouldn't need to modify this function."
+  (package-initialize)
+  (meso/user-init)
+  (add-to-list 'load-path (concat user-emacs-directory "src/"))
+  (require 'meso)
+  (meso/user-modules)
+  (meso/user-config)
+  (when custom-file
+    (load custom-file)))
+
+;; Run the init function. Don't forget this!
+(meso/init)
+
+;;; init.el ends here
